be2f4ec75f30b8a53d529374fc0b1f211b865a06
TazwoarCommits <tazwoarlives@gmail.com>
2024-12-30 15:25:17 +0600

    nav and footer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,19 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home/Home";
 
  export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      errorElement : <p>Error</p> ,
+      element: <MainLayout></MainLayout>,
+      children : [
+        {
+            path : "/" ,
+            element : <Home></Home> ,
+        }
+      ]
     },
   ]);
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { router } from './Routes/Routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <div className='max-w-screen-2xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
