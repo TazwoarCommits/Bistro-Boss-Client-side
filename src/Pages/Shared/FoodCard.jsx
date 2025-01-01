42d8f2a857eb64aa3c66ad12c7ed441850941dcb
TazwoarCommits <tazwoarlives@gmail.com>
2025-01-02 00:45:28 +0600

    part-2 done

--- src/Pages/Shared/FoodCard.jsx
+++ src/Pages/Shared/FoodCard.jsx
@@ -8,12 +8,12 @@ const FoodCard = ({item}) => {
                 <img
                     src={image}/>
             </figure>
-            <p className="absolute top-1 right-2 py-2 mr-2 mt-2 px-4 bg-slate-900 text-white hover:text-amber-300">{price}$</p>
-            <div className="card-body">
+            <p className="absolute top-1 right-2 py-2 mr-2 mt-2 px-4 bg-slate-900 text-amber-300">{price}$</p>
+            <div className="card-body flex flex-col items-center">
                 <h2 className="card-title text-center">{name}</h2>
                 <p className="text-center">{recipe}</p>
                 <div className="card-actions justify-end ">
-                    <button className="btn btn-primary">Order Now</button>
+                    <button className="border-b-2 hover:border-2 border-amber-300  p-4 rounded-lg bg-transparent hover:bg-white/10 backdrop-blur-lg text-amber-300">Order Now</button>
                 </div>
             </div>
         </div>
