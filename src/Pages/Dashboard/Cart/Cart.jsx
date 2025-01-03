239dbc053bc24cbe51dddf3d2695d43007fe5d19
TazwoarCommits <tazwoarlives@gmail.com>
2025-01-04 01:03:08 +0600

    part-4 completely Done & implemented deleting data from the cart with cuatom hooks of tanstack and axios

--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,11 +1,42 @@
 import { MdDeleteSweep } from "react-icons/md";
 import useCart from "../../../Hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 
 const Cart = () => {
-    const [cart] = useCart();
-    const totalPrice = cart.reduce((accumulator, item) => accumulator + item.price, 0)
-    console.log(cart);
+    const [cart , refetch] = useCart();
+    const totalPrice = cart.reduce((accumulator, item) => accumulator + item.price, 0);
+    const axiosSecure = useAxiosSecure();
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch() ; 
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    }
+
+
     return (
         <div>
             <div className="flex justify-evenly">
@@ -27,9 +58,9 @@ const Cart = () => {
                     </thead>
                     <tbody>
                         {
-                            cart.map((item , idx)=> <tr key={item._id} >
+                            cart.map((item, idx) => <tr key={item._id} >
                                 <td>
-                                    <p>{idx+1}</p>
+                                    <p>{idx + 1}</p>
                                 </td>
                                 <td>
                                     <div className="flex items-center gap-3">
@@ -52,7 +83,8 @@ const Cart = () => {
                                     <p>{item.price}</p>
                                 </td>
                                 <th>
-                                    <button className="btn btn-ghost text-xl text-red-700"><MdDeleteSweep /></button>
+                                    <button onClick={() => handleDelete(item._id)}
+                                        className="btn btn-ghost text-xl text-red-700"><MdDeleteSweep /></button>
                                 </th>
                             </tr>)
                         }
