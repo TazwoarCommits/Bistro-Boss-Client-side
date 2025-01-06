7e53befc4afe10e41ebd0f4e3cf06fa51d8d1ed4
TazwoarCommits <tazwoarlives@gmail.com>
2025-01-06 23:38:17 +0600

    Create Add item form using react hook form

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,9 +1,33 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form"
 
 
 const AddItem = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+
     return (
         <div>
-            <h2>AddItemPage</h2>
+            <SectionTitle
+                subHeading={"What's New"}
+                heading={"Add an Item"}
+            ></SectionTitle>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("name")} />
+                    <select  {...register("category")} className="select select-bordered w-full max-w-xs">
+                        <option disabled selected>Select a Category</option>
+                        <option>Salad</option>
+                        <option>Pizza</option>
+                        <option>Soup</option>
+                        <option>Dessert</option>
+                        <option>Drinks</option>
+                    </select>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
