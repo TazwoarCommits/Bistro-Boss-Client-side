232900cd05d768842639c8bb8a81e222fd26e6c7
TazwoarCommits <tazwoarlives@gmail.com>
2025-01-08 23:39:59 +0600

    working on deleting a menu by admin only '

--- src/Pages/Dashboard/ManageItem/ManageItem.jsx
+++ src/Pages/Dashboard/ManageItem/ManageItem.jsx
@@ -2,10 +2,33 @@ import { FaRegEdit } from 'react-icons/fa';
 import useMenu from '../../../Hooks/useMenu';
 import SectionTitle from './../../../components/SectionTitle/SectionTitle';
 import { MdDeleteSweep } from 'react-icons/md';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
+
 
 const ManageItem = () => {
     const [menues]= useMenu();
-    console.log(menues);
+    const axiosSecure = useAxiosSecure() ;
+    
+    const handleDelete = menu => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          });
+    }
     return (
         <div>
             <SectionTitle
@@ -28,8 +51,7 @@ const ManageItem = () => {
                         </thead>
                         <tbody>
                          {
-                           menues.map((menu , idx) => <>
-                               <tr>
+                           menues.map((menu , idx) => <tr key={idx}>
                                 <th>
                                     {idx+1}
                                 </th>
@@ -51,10 +73,10 @@ const ManageItem = () => {
                                     <button className="btn btn-ghost btn-xs">details <FaRegEdit className='text-xl text-amber-200'/></button>
                                 </th>
                                 <th>
-                                    <button className="btn btn-ghost btn-xs">delete <MdDeleteSweep  className='text-2xl text-red-800'/></button>
+                                    <button onClick={()=>{handleDelete(menu)}} className="btn btn-ghost btn-xs">delete <MdDeleteSweep  className='text-2xl text-red-800'/></button>
                                 </th>
                             </tr>
-                           </>)
+                          )
                          }
                         </tbody>
                     </table>
